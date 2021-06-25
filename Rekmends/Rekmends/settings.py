545419909488ae798b17a081c4b2e59949aaf973
vivek singh <65790028+vivek147-cyber@users.noreply.github.com>
2021-06-25 09:29:08 +0530

    Update settings.py

--- Rekmends/Rekmends/settings.py
+++ Rekmends/Rekmends/settings.py
@@ -148,6 +148,4 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "rekmends@gmail.com"
-EMAIL_HOST_PASSWORD = "cycletrack412020"
-DEFAULT_FROM_EMAIL  = "rekmends@gmail.com"
+
